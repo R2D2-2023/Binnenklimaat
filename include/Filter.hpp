172f68f33d282948d1a9f1de5c333c957b6ba85c
laura-ebs <74971564+laura-ebs@users.noreply.github.com>
2023-06-29 13:16:45 +0200

    Update Filter.hpp
    added doxygen documentation

--- include/Filter.hpp
+++ include/Filter.hpp
@@ -5,14 +5,46 @@
 class Filter
 {
 private:
+    /**
+     * @brief 
+     * Funtion that calculates the average
+     */
     void calcAvg();
+    /**
+     * @brief 
+     * The average
+     */
     float average;
+      /**
+     * @brief 
+     * The last average
+     */
     float last_average = -1;
+       /**
+     * @brief 
+     * Array with 5 measurements
+     */
     float measurements[5];
+       /**
+     * @brief 
+     * The amount of data point
+     */
     unsigned int data_point_count = 0;
-    static int sort_desc(const void *cmp1, const void *cmp2);
+   
 public:
+
+       /**
+     * @brief 
+     * This function calculates the datapoint 
+     */
     float getValue();
+
+       /**
+     * @brief 
+     * Adds the datapoint to measurements
+     * @param
+     * The data get value returned
+     */
     void addDatapoint(float data);
 };
 
